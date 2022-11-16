584a5bfcdbd9c2d155318a403a68fbe76521532b
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-11-15 22:51:28 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-const axios = require('axios').default;
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// const axios = require('axios').default;
