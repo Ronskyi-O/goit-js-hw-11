b4542209d31ce0c1d6e740b6c9d7d1b5e63b4322
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-11-16 22:15:14 -0500

    fixed api-service

--- src/index.js
+++ src/index.js
@@ -3,22 +3,33 @@ import {fetchImages} from './js/api-service'
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import SimpleLightbox from "simplelightbox";
 // import "simplelightbox/dist/simple-lightbox.min.css";
-// const axios = require('axios').default;
 
 let getEl = selector => document.querySelector(selector)
 let searchImageName = ''
+let pageCounter = 1
 
 getEl('#search-form').addEventListener('submit', onImputSabmit);
 getEl('.load-more').addEventListener('click', loadMore)
+
+
 function onImputSabmit(event) {
     event.preventDefault();
     searchImageName = event.currentTarget.elements.searchQuery.value;
 
-   fetchImages(searchImageName)
+    resetPagecounter()
+    fetchImages(searchImageName, pageCounter)
+    incrementPageCounter()
 }
-
 
 function loadMore(event) {
-    fetchImages(searchImageName)
+    fetchImages(searchImageName, pageCounter)
+     incrementPageCounter()
 }
 
+function incrementPageCounter() {
+    pageCounter++;
+}
+
+function resetPagecounter() {
+pageCounter = 1
+}
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,11 +1,13 @@
 const axios = require('axios').default;
 
-export async function fetchImages(searchImageName) {
-    const url = `https://pixabay.com/api/?key=31359912-5b88546d239f41508b7e3830d&q=${searchImageName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
+export async function fetchImages(searchImageName, page) {
+    const url = `https://pixabay.com/api/?key=31359912-5b88546d239f41508b7e3830d&q=${searchImageName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     try {
-        const dataResponce = await axios.get(url);
-        return dataResponce.data;
+        const response = await axios.get(url);
+        const dataResponse = response.data.hits;
+        return dataResponse;
     } catch(error) {
 console.log(error);
     }
-    }
+
+}  
