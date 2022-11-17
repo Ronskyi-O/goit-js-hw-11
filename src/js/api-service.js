c31a790a4dd1b991b96c5cb4ccbd57ab55d83d09
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-11-16 21:18:19 -0500

    added axios

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,6 +1,11 @@
-export function fetchImages(searchImageName) {
+const axios = require('axios').default;
+
+export async function fetchImages(searchImageName) {
     const url = `https://pixabay.com/api/?key=31359912-5b88546d239f41508b7e3830d&q=${searchImageName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
-    return fetch(url)
-            .then(response => response.json())
-            .then(console.log);
+    try {
+        const dataResponce = await axios.get(url);
+        return dataResponce.data;
+    } catch(error) {
+console.log(error);
+    }
     }
